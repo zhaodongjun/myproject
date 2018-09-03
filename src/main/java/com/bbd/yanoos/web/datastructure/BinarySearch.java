479df7270f46c718a5924cf16ada872d66ce90e9
zhaodongjun <zhaodongjun@bbdservice.com>
2018-09-03 16:16:17 +0800

    add 二分查找

--- src/main/java/com/bbd/yanoos/web/datastructure/BinarySearch.java
+++ src/main/java/com/bbd/yanoos/web/datastructure/BinarySearch.java
@@ -1,5 +1,7 @@
 package com.bbd.yanoos.web.datastructure;
 
+import java.util.Arrays;
+
 /**
  * 二分查找
  * Created by zhaodongjun on 2018/9/3 0003.
@@ -54,10 +56,129 @@ public class BinarySearch {
         }
     }
 
+    // 查找第一个相等的元素
+    static int findFirstEqual(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        // 这里必须是 <=
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] >= key) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        if (left < array.length && array[left] == key) {
+            return left;
+        }
+
+        return -1;
+    }
+
+    // 查找最后一个相等的元素
+    static int findLastEqual(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        // 这里必须是 <=
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] <= key) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        if (right >= 0 && array[right] == key) {
+            return right;
+        }
+
+        return -1;
+    }
+
+    // 查找最后一个等于或者小于key的元素
+    static int findLastEqualSmaller(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        // 这里必须是 <=
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] > key) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
+    // 查找最后一个小于key的元素
+    static int findLastSmaller(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        // 这里必须是 <=
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] >= key) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
+    // 查找第一个等于或者大于key的元素
+    static int findFirstEqualLarger(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        // 这里必须是 <=
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] >= key) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    // 查找第一个大于key的元素
+    static int findFirstLarger(int[] array, int key) {
+        int left = 0;
+        int right = array.length - 1;
+
+        // 这里必须是 <=
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] > key) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args) {
 //        int[] arr = { 6, 12, 33, 87, 90, 97, 108, 561 };
 //        System.out.println("循环查找：" + binarySearch(arr, 12));
 //        System.out.println("递归查找："+binarySearch(arr,12,0,arr.length-1));
+        int[] arr = { 6, 12, 33, 33, 90, 90, 108, 561 };
+        System.out.println("最左边："+findFirstEqual(arr,33));
+
 
     }
 }
