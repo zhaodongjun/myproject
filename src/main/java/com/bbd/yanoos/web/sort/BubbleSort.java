7e763eb3a7226c8222f930f95cf8558ca5635793
zhaodongjun <zhaodongjun@bbdservice.com>
2018-09-10 09:32:45 +0800

    add BubbleSort

---
+++ src/main/java/com/bbd/yanoos/web/sort/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.bbd.yanoos.web.sort;
+
+/**
+ * Created by zhaodongjun on 2018/9/6 0006.
+ */
+public class BubbleSort {
+    static int[] bubbleSort(int[] arr){
+        int length = arr.length;
+        for(int i=0;i<length;i++){
+            for(int j=0;j<length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,8,7,3,6,1,4};
+        System.out.println("arr[]="+bubbleSort(arr));
+    }
+}
+
