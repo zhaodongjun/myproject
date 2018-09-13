2faaa86f5f573b07b84da9fc34905c8ebec1691f
zhaodongjun <zhaodongjun@bbdservice.com>
2018-09-13 18:28:37 +0800

    add BubbleSortTest

---
+++ src/main/java/com/bbd/yanoos/web/sort/BubbleSortTest.java
@@ -0,0 +1,21 @@
+package com.bbd.yanoos.web.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhaodongjun on 2018/9/13 0013.
+ */
+public class BubbleSortTest {
+    static int[] bubbleSort(int[] arr){
+        int length = arr.length;
+        for(int i=0;i<length;i++){
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,8,7,3,6,1,4};
+        System.out.println("arr[]="+ Arrays.toString(bubbleSort(arr)));
+    }
+}
