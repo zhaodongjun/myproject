7f4d556005a4043bcc43a1e471e736e3b652bbf0
zhaodongjun <zhaodongjun@bbdservice.com>
2018-10-10 11:40:10 +0800

    add maxProfit1

--- src/main/java/com/bbd/yanoos/web/algorithm/Solution.java
+++ src/main/java/com/bbd/yanoos/web/algorithm/Solution.java
@@ -13,15 +13,13 @@ public class Solution {
     }
 
     public static int maxProfit(int[] prices) {
-        if(prices==null || prices.length==1){
+        if(prices==null || prices.length==0 || prices.length==1){
             return 0;
         }
         int maxProfit = 0;
-        int min = 0;
-        int max = 0;
-        for(int i=0;i<prices.length-1;i++){
-            if(prices[i]<prices[i+1]){
-
+        for(int i=1;i<prices.length;i++){
+            if(prices[i-1]<=prices[i]){
+                maxProfit = maxProfit+prices[i]-prices[i-1];
             }
         }
         return maxProfit;
