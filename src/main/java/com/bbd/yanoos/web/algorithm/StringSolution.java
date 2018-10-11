a689a167dbb18e39e052d8c905d9abc338482ac0
zhaodongjun <zhaodongjun@bbdservice.com>
2018-10-11 16:26:57 +0800

    add reverseString2

--- src/main/java/com/bbd/yanoos/web/algorithm/StringSolution.java
+++ src/main/java/com/bbd/yanoos/web/algorithm/StringSolution.java
@@ -1,12 +1,29 @@
 package com.bbd.yanoos.web.algorithm;
 
+import java.util.Arrays;
+
 /**
  * Created by zhaodongjun on 2018/10/11 0011.
  */
 public class StringSolution {
     public static void main(String[] args) {
         String input = "A man, a plan, a canal: Panama";
-        System.out.println("output="+reverseString1(input));
+        System.out.println("output="+reverseString2(input));
+    }
+
+    public static String reverseString2(String s) {
+        if(s==null||"".equals(s)){
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        int left=0;
+        int right=chars.length-1;
+        while (left<right){
+            char c = chars[left];
+            chars[left++]=chars[right];
+            chars[right--]=c;
+        }
+        return String.valueOf(chars);
     }
 
     public static String reverseString1(String s) {
